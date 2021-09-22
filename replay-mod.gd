a03751f050aec7f349c435f866402aed0cf36859
ty <fqdingsky@gmail.com>
2021-09-22 16:57:05 +0800

    bug fixes (loading entire game playthroughs and some stuff)

--- replay-mod.gd
+++ replay-mod.gd
@@ -18,7 +18,7 @@ var loadedpositions_y = []
 
 var ii = 0
 
-var replaying
+var replaying = false
 
 var loadedreplay = File.new()
 var loadingreplay = File.new()
@@ -26,13 +26,15 @@ var f7_key = InputEventKey.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	replaying = false
 	f7_key.scancode = KEY_F7
 	self.visible = false
 	InputMap.add_action("replay_menu")
 	InputMap.action_add_event("replay_menu", f7_key)
 	base = get_node("/root/base")
-	print(base.menu.connect("end_text", self, "save_replay"))
+	base.menu.connect("end_text", self, "save_replay")
+	base.menu.connect("mainmenu", self, "end_replay")
 	player = base.player
 	var dir =  Directory.new()
 	dir.open("user://")
@@ -54,6 +56,10 @@ func _ready():
 	for replay in replays:
 		$OptionButton.add_item(str(replay).trim_suffix(".ssreplay"))
 
+func end_replay():
+	ii = 0
+	replaying = false
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if base.state == "play":
@@ -67,12 +73,15 @@ func _process(delta):
 			base.menu._hideall()
 
 func _physics_process(_delta):
-	if replaying:
+	if replaying && base.state == "play":
 		player.position = Vector2(loadedpositions_x[ii], loadedpositions_y[ii])
 		ii += 1
 
 func save_replay():
-	level = base.level
+	if base.mode == "level":
+		level = base.level
+	else:
+		level = -1
 	var replay = File.new()
 	var timeDict = OS.get_datetime()
 	var savetime = str(timeDict["hour"] , "-" , timeDict["minute"] , "-" , timeDict["second"] , "-" , timeDict["day"] , "-" , timeDict["month"] , "-" , timeDict["year"])
@@ -95,5 +104,8 @@ func _on_open_pressed():
 	loadedpositions_x = parse_json(loadedreplay.get_line())
 	loadedpositions_y = parse_json(loadedreplay.get_line())
 	self.visible = false
-	base._playlevel(loadedlevel)
+	if loadedlevel == -1:
+		base._play()
+	else:
+		base._playlevel(loadedlevel)
 	replaying = true
